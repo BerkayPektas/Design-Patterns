f40823a3aab361848afd3e30f15fbe2213711034
1197777 <1197777@student.roc-nijmegen.nl>
2024-11-14 12:02:41 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using StrategyPattern.Ducks;
+using StrategyPattern.Interfaces.FlyBehavior;
+using System.Xml.Schema;
+
+namespace StrategyPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Duck mallardDuck = new MallardDuck();
+            mallardDuck.Display();
+            mallardDuck.PerformQuack();
+            mallardDuck.PerformFly();
+            mallardDuck.PerformSwim();
+
+            Duck redheadDuck = new RedheadDuck();
+            redheadDuck.Display();
+            redheadDuck.PerformQuack();
+            redheadDuck.PerformFly();
+            redheadDuck.PerformSwim();
+
+            Duck decoyDuck = new DecoyDuck();
+            decoyDuck.Display();
+            decoyDuck.PerformQuack();
+            decoyDuck.PerformFly();
+            decoyDuck.PerformSwim();
+
+            Duck rubberDuck = new RubberDuck();
+            rubberDuck.Display();
+            rubberDuck.PerformQuack();
+            rubberDuck.PerformFly();
+            rubberDuck.PerformSwim();
+
+            Duck robotDuck = new RobotDuck();
+            robotDuck.Display();
+            robotDuck.PerformQuack();
+            robotDuck.PerformFly();
+            robotDuck.SetFlybehavior(new FlyRocketPowered());
+            robotDuck.PerformFly();
+            robotDuck.PerformSwim();
+
+           
+            
+
+
+
+
+        }
+    }
+}

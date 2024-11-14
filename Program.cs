4f7a59ef03b5e9738f75e7e78a598d7e6abf67b8
1197777 <1197777@student.roc-nijmegen.nl>
2024-11-14 13:34:29 +0100

    Singleton-Pattern

--- Program.cs
+++ Program.cs
@@ -1,51 +1,10 @@
-﻿using StrategyPattern.Ducks;
-using StrategyPattern.Interfaces.FlyBehavior;
-using System.Xml.Schema;
-
-namespace StrategyPattern
+﻿namespace Singleton
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Duck mallardDuck = new MallardDuck();
-            mallardDuck.Display();
-            mallardDuck.PerformQuack();
-            mallardDuck.PerformFly();
-            mallardDuck.PerformSwim();
-
-            Duck redheadDuck = new RedheadDuck();
-            redheadDuck.Display();
-            redheadDuck.PerformQuack();
-            redheadDuck.PerformFly();
-            redheadDuck.PerformSwim();
-
-            Duck decoyDuck = new DecoyDuck();
-            decoyDuck.Display();
-            decoyDuck.PerformQuack();
-            decoyDuck.PerformFly();
-            decoyDuck.PerformSwim();
-
-            Duck rubberDuck = new RubberDuck();
-            rubberDuck.Display();
-            rubberDuck.PerformQuack();
-            rubberDuck.PerformFly();
-            rubberDuck.PerformSwim();
-
-            Duck robotDuck = new RobotDuck();
-            robotDuck.Display();
-            robotDuck.PerformQuack();
-            robotDuck.PerformFly();
-            robotDuck.SetFlybehavior(new FlyRocketPowered());
-            robotDuck.PerformFly();
-            robotDuck.PerformSwim();
-
-           
-            
-
-
-
-
+            ChocolateBoiler chocolateBoiler = ChocolateBoiler.GetInstance();
         }
     }
 }
